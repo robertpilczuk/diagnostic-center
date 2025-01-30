73875d0ff276c6b6543b158fa1e582e86b0a31d4
robertpilczuk <robertpilczuk@gmail.com>
2025-01-30 18:49:25 +0100

    Create registration and login views in accounts app

--- diagnostic_center/accounts/views.py
+++ diagnostic_center/accounts/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from .forms import UserRegistrationForm, UserLoginForm
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = UserRegistrationForm()
+    return render(request, "accounts/register.html", {"form": form})
+
+
+def user_login(request):
+    if request.method == "POST":
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password"],
+            )
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+    else:
+        form = UserLoginForm()
+    return render(request, "accounts/login.html", {"form": form})
+
 
 # Create your views here.
