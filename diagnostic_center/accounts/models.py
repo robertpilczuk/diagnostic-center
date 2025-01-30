576f876c45fe7396f8494da40f5c5fa0c5360730
robertpilczuk <robertpilczuk@gmail.com>
2025-01-30 18:21:49 +0100

    Define User model in accounts app

--- diagnostic_center/accounts/models.py
+++ diagnostic_center/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class User(AbstractUser):
+    is_admin = models.BooleanField(default=False)
+    is_laboratory = models.BooleanField(default=False)
+    is_doctor = models.BooleanField(default=False)
+    is_patient = models.BooleanField(default=False)
