8ba1eda180a1e4b85c61d06fa4666525b674fea1
robertpilczuk <robertpilczuk@gmail.com>
2025-01-30 19:36:44 +0100

    Define LabTest and AppointmentRequest models in laboratory app

--- diagnostic_center/laboratory/models.py
+++ diagnostic_center/laboratory/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from accounts.models import User
+
+
+class LabTest(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    laboratory = models.ForeignKey(
+        User, on_delete=models.CASCADE, limit_choices_to={"is_laboratory": True}
+    )
+
+
+class AppointmentRequest(models.Model):
+    patient = models.ForeignKey(
+        User, on_delete=models.CASCADE, limit_choices_to={"is_patient": True}
+    )
+    laboratory = models.ForeignKey(
+        User, on_delete=models.CASCADE, limit_choices_to={"is_laboratory": True}
+    )
+    date = models.DateTimeField()
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ("pending", "Pending"),
+            ("accepted", "Accepted"),
+            ("rejected", "Rejected"),
+        ],
+    )
+
 
 # Create your models here.
