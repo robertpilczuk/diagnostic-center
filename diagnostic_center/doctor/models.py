90f11898692eb340817c5f29f100c6df6e6f2b9b
robertpilczuk <robertpilczuk@gmail.com>
2025-01-31 14:04:18 +0100

    Define Prescription model in doctor app

--- diagnostic_center/doctor/models.py
+++ diagnostic_center/doctor/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from accounts.models import User
+
+
+class Prescription(models.Model):
+    doctor = models.ForeignKey(
+        User, on_delete=models.CASCADE, limit_choices_to={"is_doctor": True}
+    )
+    patient = models.ForeignKey(
+        User, on_delete=models.CASCADE, limit_choices_to={"is_patient": True}
+    )
+    description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
 
 # Create your models here.
