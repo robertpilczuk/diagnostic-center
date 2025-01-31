26dfa5bf2e69c385e9f79cdfee19d6a467553459
robertpilczuk <robertpilczuk@gmail.com>
2025-01-31 14:24:58 +0100

    Create views for searching patients and writing prescriptions in doctor app

--- diagnostic_center/doctor/views.py
+++ diagnostic_center/doctor/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PrescriptionForm
+from .models import Prescription
+
+
+def search_patient(request):
+    if request.method == "POST":
+        query = request.POST.get("query")
+        patients = User.objects.filter(is_patient=True, username__icontains=query)
+        return render(request, "doctor/search_patient.html", {"patients": patients})
+    return render(request, "doctor/search_patient.html")
+
+
+def write_prescription(request):
+    if request.method == "POST":
+        form = PrescriptionForm(request.POST)
+        prescription = form.save(commit=False)
+        prescription.doctor = request.user
+        prescription.save()
+        return redirect("prescription_list")
+    else:
+        form = PrescriptionForm()
+    return render(request, "doctor/write_prescription.html", {"form": form})
+
 
 # Create your views here.
