4d14b334fb7b4ebf68f65f99559bce113d746f3d
robertpilczuk <robertpilczuk@gmail.com>
2025-01-31 13:32:21 +0100

    Update references to laboratory app

--- diagnostic_center/laboratory/apps.py
+++ diagnostic_center/laboratory/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LabolatoryConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'labolatory'
+class LaboratoryConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "laboratory"
