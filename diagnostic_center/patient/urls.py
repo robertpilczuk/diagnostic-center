33d0111c102674e69f1156605e669cbb160ee7c7
robertpilczuk <robertpilczuk@gmail.com>
2025-02-07 13:32:52 +0100

    Fix patient registration by adding URL

--- diagnostic_center/patient/urls.py
+++ diagnostic_center/patient/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("register/patient/", views.patient_register, name="patient_register"),
     path("patient_register", views.patient_register, name="patient_register"),
     path("view_tests/", views.view_tests, name="view_tests"),
     path("book_appointment/", views.book_appointment, name="book_appointment"),
